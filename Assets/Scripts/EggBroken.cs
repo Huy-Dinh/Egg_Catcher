13696853a69f568ccec99d8554ab69d100fb66ca
Unknown <vohoangkhoi1211@gmail.com>
2019-06-09 22:51:44 +0200

    Broken Egg Effects
    Swap egg to broken egg when meet the grass, set delay time to destroy, delete rigidbody2D and Collider after colliding, set bounciness to 0 after colliding

---
+++ Assets/Scripts/EggBroken.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EggBroken : MonoBehaviour
+{
+    // Get the SpriteRenderer of the Egg
+    SpriteRenderer thisSpriteRenderer;
+
+    void OnCollisionEnter2D(Collision2D col)
+    {
+        if (col.gameObject.tag == "Grass")
+        {  
+            thisSpriteRenderer = GetComponent<SpriteRenderer>();
+            var sprite = Resources.Load<Sprite>("EggBroken");
+            thisSpriteRenderer.sprite = sprite;
+
+            CircleCollider2D currentCollider = GetComponent<CircleCollider2D>();
+            PhysicsMaterial2D material = new PhysicsMaterial2D();
+            material.friction = currentCollider.sharedMaterial.friction;
+            material.bounciness = 0;
+            currentCollider.sharedMaterial = material;
+
+            Rigidbody2D currentRigidBody = GetComponent<Rigidbody2D>();
+            currentRigidBody.velocity = new Vector2(0, 0);
+            currentCollider.enabled = false;
+            Destroy(currentRigidBody);
+            Destroy(gameObject, 2);
+        }
+    }
+
+
+}
