4526f85283cda0a3c94da3c2fd9513b208b9afcd
DATHC <avaritia.95@gmail.com>
2019-09-15 17:13:16 +0200

    Add new asset

---
+++ Assets/Scripts/EggComponents/OnCaught/AffectTimerOnCaught.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AffectTimerOnCaught : MonoBehaviour
+{
+    public int pointAdded;
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.gameObject.tag == "Hat")
+        {
+            TimerGameController.timeLeft = TimerGameController.timeLeft + pointAdded;
+        }        
+    }
+}
