db616f75a60cb4807bea62f5f82ddd3f3e8a0a66
DATHC <avaritia.95@gmail.com>
2019-09-15 17:13:42 +0200

    Add function to lay Time Egg

--- Assets/Scripts/EggLaying.cs
+++ Assets/Scripts/EggLaying.cs
@@ -8,6 +8,7 @@ public class EggLaying : MonoBehaviour
     public GameObject egg;
     public GameObject lifeEgg;
     public GameObject virusEgg;
+    public GameObject timeEgg;
     private AudioSource source;
     private Object[] drop;
     private Object[] chicken;
@@ -82,6 +83,27 @@ public class EggLaying : MonoBehaviour
             source.PlayOneShot(sound);
             transform.parent.transform.GetComponent<Animator>().SetTrigger("givingBirth");
             Instantiate(lifeEgg, spawnPosition, spawnRotation);
+        }
+    }
+
+    public void LayTimeEgg()
+    {
+        if (timeEgg != null)
+        {
+            Vector3 spawnPosition = new Vector3(
+                transform.position.x,
+                transform.position.y,
+                0.0f
+                );
+            Quaternion spawnRotation = Quaternion.identity;
+
+            // Play sound
+            var sound = (AudioClip)chicken[Random.Range(0, chicken.Length)];
+            source.PlayOneShot(sound, (float)0.5);
+            sound = (AudioClip)drop[Random.Range(0, drop.Length)];
+            source.PlayOneShot(sound);
+            transform.parent.transform.GetComponent<Animator>().SetTrigger("givingBirth");
+            Instantiate(timeEgg, spawnPosition, spawnRotation);
         }
     }
 }
